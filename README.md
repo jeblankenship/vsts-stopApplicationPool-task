af20632b5e12ca50213ca3de4ead9a34023140b1
James Blankenship <jeblankenship@gmail.com>
2016-10-21 14:15:10 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Stop Application Pool Task
+Build task for VS Team Services that stops the Application Pool on the specified server.
+
+
+10/21/2016: First Beta.
---
+++ task/stop-applicationPool.ps1
@@ -0,0 +1,35 @@
+﻿[CmdletBinding()]
+param(
+    [string]$server,
+    [string]$appPoolName
+)
+
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+Write-Verbose "Entering script $($MyInvocation.MyCommand.Name)"
+Write-Verbose "Parameter Values"
+$PSBoundParameters.Keys | % { Write-HOST "  ${_} = $($PSBoundParameters[$_])" }
+$command = {
+    param(
+        [string]$appPoolName
+    )
+    Import-Module WebAdministration
+    Write-Host "Stop Application Pool Task: Starting..."
+    $appPoolPath = "IIS:\AppPools\$AppPoolName"    
+    if(Test-Path $appPoolPath) {
+        Write-Host "Checking Application Pool State..."
+	    $ApplicationPoolStatus = Get-WebAppPoolState $appPoolName
+        if ($ApplicationPoolStatus.Value -ne "Stopped")
+	    {
+            Write-Host "Stopping application pool: $appPoolName"
+            Stop-WebAppPool -Name $appPoolName
+	    } else {
+            Write-Host "Application pool already stopped."
+        }
+    } else {
+        Write-Host "Application pool not found: $appPoolName"
+    }
+    Write-Host "Application Pool Stop Task: Completed"
+}
+Invoke-Command -ComputerName $server $command -ArgumentList $appPoolName
